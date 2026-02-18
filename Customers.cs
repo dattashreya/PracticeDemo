e5651f3695dbf05f6869768d63c0f34e2de6e96a
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-18 17:16:42 +0530

    Constructor overloading in C#
    Constructor overloading in C#

---
+++ Customers.cs
@@ -0,0 +1,11 @@
+public class Customers {
+    public string Name;
+    public int Id;
+    public Customers(string name) {
+        this.Name = name;
+    }
+    public Customers(int id,string name) {
+        this.Id = id;
+        this.Name = name;
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System;
+class Program {
+  static void Main() {
+    var obj = new Customers(10,"Shreya");
+    Console.WriteLine(obj.Name);
+    Console.WriteLine(obj.Id);
+  } 
+}
