1ddeebf506bd72b60def781c661648b2bbe87c66
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-20 18:46:26 +0530

    Add Customer1 class with basic structure
    Created Customer1 class with properties and methods.

---
+++ Customer1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+class Customer1 {
+    public int Id;
+    public string Name;
+    public List<Order> Orders = new List<Order>();
+
+    public Customer(int id) 
+    {
+        this.Id = id;
+    }
+    public Customer(int id, string name) 
+        : this(id)
+    {
+        this.Name = name;
+    }
+    public void Promote()
+    {
+        Orders = new List<Order>();
+        //
+    }
+}
