fe9e06b09aa6ebfad7ceec296fe3de07c52a1ab8
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-20 20:24:50 +0530

    Add Dog class implementing IAnimal interface

---
+++ Dog.cs
@@ -0,0 +1,11 @@
+public class Dog : IAnimal
+{
+    public void bark()
+    {
+        Console.WriteLine("Barking");
+    }
+    public void run()
+    {
+        Console.WriteLine("Running");
+    }
+}
