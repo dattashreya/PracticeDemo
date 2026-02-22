54a8e6b337931c5678861e379bbf2ab0d715e27a
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-22 21:26:18 +0530

    enum demo with days
    enum demo with days

---
+++ MyEnumDemo1.cs
@@ -0,0 +1,16 @@
+using System;
+public enum Days {
+    Monday, 
+    Tuesday, 
+    Wednesday, 
+    Thursday, 
+    Friday
+}
+class MyEnumDemo1 {
+    static void Main() {
+        // value typed category
+        Days d = 0;
+        Console.WriteLine(d);
+  }
+}
+
